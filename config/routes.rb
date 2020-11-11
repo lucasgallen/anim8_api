3ba271d274085579af8db2363585e59a47f7ecb7
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-11 10:17:40 +0000

    Add routes to retreive/create session_groups and patch shared_images

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      get   'session_groups/new_session', to: 'session_group#new_session'
+      get   'session_group/:slug', to: 'session_group#show', as: 'session_group'
+      patch 'shared_image/:slug', to: 'shared_image#update', as: 'shared_image_update'
+    end
+  end
 end
