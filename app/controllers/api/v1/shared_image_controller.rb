e9220f79e25a9d18f5517462f76b57badd080717
Lucas Allen <lucas.g.allen@gmail.com>
2021-01-25 15:55:32 +0000

    Generalize means of updating shared images from params

--- app/controllers/api/v1/shared_image_controller.rb
+++ app/controllers/api/v1/shared_image_controller.rb
@@ -8,7 +8,7 @@ module Api
 
         if session.blank? || session.shared_image.blank?
           render json: {}, status: 404
-        elsif session.shared_image.update!(data_url: shared_image_params[:data_url])
+        elsif session.shared_image.update!(attributes)
           session.touch(:image_updated_at)
           render jsonapi: session.shared_image, status: 200
         else
@@ -18,8 +18,14 @@ module Api
 
       private
 
+      def attributes
+        attrs = [:data_url, :colors]
+        attrs.map{ |attr| [attr, shared_image_params[:shared_image][attr]] }
+             .to_h
+      end
+
       def shared_image_params
-        params.permit(:data_url, :slug)
+        params.permit(:slug, shared_image: [:data_url, :colors])
       end
 
       def restrict_access
